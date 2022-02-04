197168b3fa007a0507a821822479b46390873ce6
Sseast <ayadguillaume@gmail.com>
2022-02-04 18:14:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import "./App.css";
+
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/signin" element={<SignInAndSignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
